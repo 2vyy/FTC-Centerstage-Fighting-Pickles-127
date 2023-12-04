e188f6b8529994ecbe4e34ce95a9704fd3e614a0
Jared <thatjaredbrown@gmail.com>
2023-12-04 17:14:58 -0600

    optimized imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_NewBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_NewBot.java
@@ -3,8 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
+remimport com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="TeleOp_NewBot", group="Linear Opmode")
